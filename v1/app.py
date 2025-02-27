0e4ab9c4b1d692fbc9b5e7d752e9bdea7512c1d5
Diego Gabriel Zaldivar Tovar <116207918+diegogzt@users.noreply.github.com>
2025-02-27 01:36:11 +0100

    Update app.py

--- v1/app.py
+++ v1/app.py
@@ -85,6 +85,8 @@ app.secret_key = 'supersecretkey'  # Necesario para usar sesiones
 @app.route("/")
 def home():
     return render_template('index.html')  # Sirve el archivo index.html desde la carpeta templates
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # Ruta para manejar las solicitudes del chatbot
 @app.route("/chat", methods=["POST"])
